1085ca008f8bbfc225356cf32e3b6ee2bac1124c
TypingMonkey <cosmicthermo@outlook.com>
2022-08-29 16:09:50 +0100

    completed first part of lab1

---
+++ lab1/HelloNumbers.java
@@ -0,0 +1,12 @@
+public class HelloNumbers {
+	public static void main(String[] args) {
+		int x = 0;
+		int result = 0;
+		while (x < 10) {
+			
+			result += x;
+			System.out.print(result + " ");
+			x++;
+}
+}
+}
