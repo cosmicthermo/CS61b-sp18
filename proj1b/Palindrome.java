798baa10dbdb60e0910ed7e89a1c8362da2cccfd
TypingMonkey <cosmicthermo@outlook.com>
2022-10-20 13:41:07 +0100

    Proj1b task3.

--- proj1b/Palindrome.java
+++ proj1b/Palindrome.java
@@ -2,4 +2,31 @@
  * A class for palindrome operations
  */
 public class Palindrome {
+
+    public Deque<Character> wordToDeque(String word) {
+        Deque<Character> deque = new LinkedListDeque<>();
+
+        for (int i = 0; i < word.length(); i += 1) {
+            deque.addLast(word.charAt(i));
+        }
+        return deque;
+    }
+
+
+    public boolean isPalindrome(String word) {
+        if (word.length() <= 1) {
+            return true;
+        }
+        Deque<Character> deque = wordToDeque(word);
+        return isPalindromeHelper(deque);
+    }
+
+    private boolean isPalindromeHelper(Deque<Character> deque) {
+        if (deque.size() <= 1) {
+            return true;
+        }
+        return deque.removeFirst().equals(deque.removeLast()) && isPalindromeHelper(deque);
+    }
+
+
 }
